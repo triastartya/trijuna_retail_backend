5a842f0a91cd44872bff67786b18f758ce488e4e
triastartya <triastartya@gmail.com>
2024-04-07 03:00:01 +0700

    build

---
+++ build.sh
@@ -0,0 +1,8 @@
+git pull
+composer update
+composer dump-autoload
+php artisan optimize:clear
+docker build --no-cache -t triastartya/api-retail:$(date '+%d/%m/%Y%H:%M:%S')
+docker push triastartya/api-retail:$(date '+%d/%m/%Y%H:%M:%S')
+docker tag triastartya/api-retail:$(date '+%d/%m/%Y%H:%M:%S') triastartya/api-retail:dev-latest
+docker push triastartya/api-retail:dev-latest
