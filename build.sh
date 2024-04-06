a9ca29d89ee988091f06b8354825b731f0e66e7e
triastartya <triastartya@gmail.com>
2024-04-07 03:14:24 +0700

    j

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull
-docker build --no-cache -t triastartya/api-retail:$(date '+%d%m%Y%H%M%S')
-docker push triastartya/api-retail:$(date '+%d%m%Y%H%M%S')
-docker tag triastartya/api-retail:$(date '+%d%m%Y%H%M%S') triastartya/api-retail:dev-latest
+docker build --no-cache -t triastartya/api-retail:$(date '+%d%m%Y%H%M')
+docker push triastartya/api-retail:$(date '+%d%m%Y%H%M')
+docker tag triastartya/api-retail:$(date '+%d%m%Y%H%M') triastartya/api-retail:dev-latest
 docker push triastartya/api-retail:dev-latest
