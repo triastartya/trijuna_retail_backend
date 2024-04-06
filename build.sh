a75bb3325a0ff2029119afa8eac553bcfc7eb04c
triastartya <triastartya@gmail.com>
2024-04-07 03:49:53 +0700

    k

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 git pull origin development
-uuid=uuidgen
+uuid=$(date '+%d%m%Y%H%M%S');
 docker build --no-cache -t triastartya/api-retail:$uuid .
 docker push triastartya/api-retail:$uuid
 docker tag triastartya/api-retail:$uuid triastartya/api-retail:latest
