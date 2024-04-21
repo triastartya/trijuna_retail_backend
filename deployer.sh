924f45be6b246a96260dcf0fefd9deb9c1d59f5f
triastartya <triastartya@gmail.com>
2024-04-21 19:58:46 +0700

    ok

--- deployer.sh
+++ deployer.sh
@@ -6,7 +6,7 @@ echo "Delete Image"
 docker rmi backend-frankenphp:latest
 
 echo "Pulling latest changes from Git..."
-git pull origin devlopment
+git pull origin development
 
 echo "Starting containers with new changes..."
 docker compose up --wait
