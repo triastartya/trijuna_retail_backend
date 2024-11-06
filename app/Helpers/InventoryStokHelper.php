8edcadf301aa4aa7bbb1fb5f36ffdd9f555c3934
triastartya <triastartya@gmail.com>
2024-11-06 23:37:08 +0700

    hitung hpp average

--- app/Helpers/InventoryStokHelper.php
+++ app/Helpers/InventoryStokHelper.php
@@ -128,7 +128,7 @@ class InventoryStokHelper
         $stok_on_hand = msBarangStok::where('id_barang',$id_barang)->sum('qty');
         $stok_persediaan = ($stok_on_hand)?$stok_on_hand:0;
         $hpp_average = ($master_barang->hpp_average)?$master_barang->hpp_average:0;
-        if($hpp_average==0 || $stok_persediaan==0){
+        if($hpp_average<=0 || $stok_persediaan<=0){
             $hpp_average = $subtotal/$qty;
         }else{
             $modal = $stok_persediaan*$hpp_average;
