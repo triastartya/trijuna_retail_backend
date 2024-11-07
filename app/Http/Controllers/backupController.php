ad591145c27ab8cc59af94b7cced0fb37aefd336
triastartya <triastartya@gmail.com>
2024-11-08 02:02:42 +0700

    post

--- app/Http/Controllers/backupController.php
+++ app/Http/Controllers/backupController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Viershaka\Vier\VierController;
+use Illuminate\Support\Facades\Storage;
 
 class backupController extends VierController
 {
@@ -20,7 +21,8 @@ class backupController extends VierController
     {
         try {
             // Run the backup command programmatically
-            Artisan::call('backup:run');
+            // Artisan::call('backup:run');
+            Storage::disk('google_drive')->put('nama_file.txt', 'Isi dari file');
 
             // Optionally, return a response
             return response()->json(['message' => 'Backup is being processed.'], 200);
