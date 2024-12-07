c47afe808767f479224d46cfe75359027860f690
triastartya <triastartya@gmail.com>
2024-12-07 11:27:22 +0700

    penerimaan

--- app/Http/Controllers/Pembelian/penerimaanDenganPOController.php
+++ app/Http/Controllers/Pembelian/penerimaanDenganPOController.php
@@ -281,13 +281,13 @@ class penerimaanDenganPOController extends VierController
             $data['sub_total1'] = $sub_total;
             $data['sub_total2'] = $sub_total - $data['diskon_nominal'];
             $data['total_transaksi'] = $data['sub_total2'] + $data['ppn_nominal'] + $data['potongan'] + $data['pembulatan'];
-            $penerimaan = trPenerimaan::where('id_penerimaan',$request->id_penerimaan)->update($data);
             trPenerimaanDetail::where('id_penerimaan',$request->id_penerimaan)->delete();
             // if($data['diskon_persen']==0){
                 if($data['diskon_nominal']!=0){
                     $data['diskon_persen'] = number_format(($data['diskon_nominal']/$data['sub_total1'])*100);
                 }
             // }
+            $penerimaan = trPenerimaan::where('id_penerimaan',$request->id_penerimaan)->update($data);
             $urut= 0;
             foreach($request->detail as $detail){
                 $urut++;
