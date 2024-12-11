6261fb56c8763285b2158c63b74f461590cb075b
triastartya <triastartya@gmail.com>
2024-12-11 22:57:43 +0700

    modal kasir

--- app/Http/Controllers/Penjualan/modalKasirController.php
+++ app/Http/Controllers/Penjualan/modalKasirController.php
@@ -23,8 +23,8 @@ class modalKasirController extends VierController
         try{
             $data = DB::select("
                 select pmk.id_modal_kasir, pmk.id_user_kasir,u.nama as nama_kasir, pmk.tanggal_modal_kasir, pmk.modal_kasir, pmk.is_deleted, pmk.deleted_by, pmk.deleted_at, pmk.deleted_reason, pmk.created_by, pmk.updated_by, pmk.id_tutup_kasir, pmk.created_at, pmk.updated_at
-                from pos_modal_kasir pmk inner join users u on pmk.id_user_kasir=u.id_user;
-            ",[]);
+                from pos_modal_kasir pmk inner join users u on pmk.id_user_kasir=u.id_user where pmk.tanggal_modal_kasir = ? ;
+            ",[date('Y-m-d')]);
             return response()->json(['success'=>true,'data'=>$data]);
         } catch (\Exception $ex) {
             return response()->json(['success'=>false,'data'=>[],'message'=>$ex->getMessage()]);
