c2f03c2dbf629b3c68aa3f00a95a96eaea1cf03c
triastartya <triastartya@gmail.com>
2024-12-14 11:16:11 +0700

    hpp average

--- app/Http/Controllers/Penjualan/penjualanController.php
+++ app/Http/Controllers/Penjualan/penjualanController.php
@@ -48,7 +48,9 @@ class penjualanController extends VierController
             $point_setting_group = msMemberPoinSettingGroup::get();
             foreach($request->detail as $detail){
                 $barang = msBarang::where('id_barang',$detail['id_barang'])->first();
-                $detail['hpp_average'] = ($barang->hpp_average)?$barang->hpp_average:0;
+                $hpp_average = ($barang)?$barang->hpp_average:0;
+                $hpp_average = ($hpp_average)?$hpp_average:0;
+                $detail['hpp_average'] = $hpp_average;
                 $detail['laba'] = $barang->harga_jual - ($barang->hpp_average)?$barang->hpp_average:0;
                 $detail['id_penjualan'] = $penjualan->id_penjualan;
                 $penjualan_detail =posPenjualanDetail::create($detail);
