ab1300b195d2f246d10ec260d8369c131844c8c6
triastartya <triastartya@gmail.com>
2024-12-17 21:36:43 +0700

    faktur pajak

--- app/Repositories/Finance/fakturPajakRepository.php
+++ app/Repositories/Finance/fakturPajakRepository.php
@@ -17,12 +17,31 @@ class fakturPajakRepository extends VierRepository
     public function by_param()
     {
         return QueryHelper::queryParam('
-            select ms.id_supplier,
-            ms.kode_supplier,
-            ms.nama_supplier,
-            ms.alamat,
-            ms.npwp,
-            tp.nomor_penerimaan,
+            select
+            CASE
+                    WHEN tfp.retur = false THEN ms.kode_supplier
+                    ELSE msr.kode_supplier
+            END AS kode_supplier,
+            CASE
+                    WHEN tfp.retur = false THEN ms.nama_supplier
+                    ELSE msr.nama_supplier
+            END AS nama_supplier,
+            CASE
+                    WHEN tfp.retur = false THEN ms.alamat
+                    ELSE msr.alamat
+            END AS alamat,
+            CASE
+                    WHEN tfp.retur = false THEN ms.npwp
+                    ELSE msr.npwp
+            END AS npwp,
+            CASE
+                    WHEN tfp.retur = false THEN ms.id_supplier
+                    ELSE msr.id_supplier
+            END AS id_supplier,
+            CASE
+                    WHEN tfp.retur = false THEN tp.nomor_penerimaan
+                    ELSE trp.nomor_retur_pembelian
+            END AS nomor_penerimaan,
             tp.tanggal_nota,
             tfp.id_faktur_pajak,
             tfp.id_penerimaan,
@@ -35,23 +54,46 @@ class fakturPajakRepository extends VierRepository
             tfp.created_at,
             uc.nama as created_by,
             tfp.updated_by,
-            uu.nama as updated_by
+            uu.nama as updated_by,
+            tfp.retur
             from tr_faktur_pajak tfp
-            inner join tr_penerimaan tp on tfp.id_penerimaan = tp.id_penerimaan
-            inner join tr_pemesanan tps on tp.id_pemesanan =tps.id_pemesanan
-            inner join ms_supplier ms on tps.id_supplier = ms.id_supplier            inner join users uc on uc.id_user = tfp.created_by
-            inner join users uu on uu.id_user = tfp.updated_by 
+            left join tr_penerimaan tp on tfp.id_penerimaan = tp.id_penerimaan
+            left join tr_pemesanan tps on tp.id_pemesanan =tps.id_pemesanan
+            left join ms_supplier ms on tps.id_supplier = ms.id_supplier
+            inner join users uc on uc.id_user = tfp.created_by
+            inner join users uu on uu.id_user = tfp.updated_by
+            left join tr_retur_pembelian trp on trp.id_retur_pembelian=tfp.id_penerimaan
+            left join ms_supplier msr on msr.id_supplier=trp.id_supplier
         ',request());
     }
 
     public function get_by_id(){
         $data = DB::select("
-            select ms.id_supplier,
-            ms.kode_supplier,
-            ms.nama_supplier,
-            ms.alamat,
-            ms.npwp,
-            tp.nomor_penerimaan,
+            select
+            CASE
+                WHEN tfp.retur = false THEN ms.kode_supplier
+                ELSE msr.kode_supplier
+            END AS kode_supplier,
+            CASE
+                    WHEN tfp.retur = false THEN ms.nama_supplier
+                    ELSE msr.nama_supplier
+            END AS nama_supplier,
+            CASE
+                    WHEN tfp.retur = false THEN ms.alamat
+                    ELSE msr.alamat
+            END AS alamat,
+            CASE
+                    WHEN tfp.retur = false THEN ms.npwp
+                    ELSE msr.npwp
+            END AS npwp,
+            CASE
+                    WHEN tfp.retur = false THEN ms.id_supplier
+                    ELSE msr.id_supplier
+            END AS id_supplier,
+            CASE
+                    WHEN tfp.retur = false THEN tp.nomor_penerimaan
+                    ELSE trp.nomor_retur_pembelian
+            END AS nomor_penerimaan,
             tp.tanggal_nota,
             tfp.id_faktur_pajak,
             tfp.id_penerimaan,
@@ -64,13 +106,16 @@ class fakturPajakRepository extends VierRepository
             tfp.created_at,
             uc.nama as created_by,
             tfp.updated_by,
-            uu.nama as updated_by
+            uu.nama as updated_by,
+            tfp.retur
             from tr_faktur_pajak tfp
-            inner join tr_penerimaan tp on tfp.id_penerimaan = tp.id_penerimaan
-            inner join tr_pemesanan tps on tp.id_pemesanan =tps.id_pemesanan
-            inner join ms_supplier ms on tps.id_supplier = ms.id_supplier
+            left join tr_penerimaan tp on tfp.id_penerimaan = tp.id_penerimaan
+            left join tr_pemesanan tps on tp.id_pemesanan =tps.id_pemesanan
+            left join ms_supplier ms on tps.id_supplier = ms.id_supplier            
             inner join users uc on uc.id_user = tfp.created_by
             inner join users uu on uu.id_user = tfp.updated_by
+			left join tr_retur_pembelian trp on trp.id_retur_pembelian=tfp.id_penerimaan
+			left join ms_supplier msr on msr.id_supplier=trp.id_supplier
             where tfp.id_faktur_pajak = ?
         ",[request()->id_faktur_pajak]);
         return $data[0];
